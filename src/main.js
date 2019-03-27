e7423e30918bef09f8a022fb75d032b3ec0dcdeb
Fireworks <1019723393@qq.com>
2019-03-27 15:23:27 +0800

    哈哈

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 全局引入样式
+import './assets/statics/site/css/style.css'
+// Vue.config.productionTip = false
+// 路由相关
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+    //导入组件
+import index from './components/index.vue'
+import cart from './components/cart.vue'
 
-Vue.config.productionTip = false
-
+//规则
+let routes = [{
+    path: '/',
+    component: index
+}, {
+    path: '/index',
+    component: index
+}, {
+    path: '/cart',
+    component: cart
+}]
+let router = new VueRouter({
+    routes
+})
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+    router
+})
